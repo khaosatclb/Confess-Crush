e37c0c80140505a75b49449fc332060ae47fc66a
khaosatclb <94673480+khaosatclb@users.noreply.github.com>
2021-11-19 14:54:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     question: 'Lý do cậu iu tớ là gì áaa',
     btnReply: 'Gửi cho tớ <3',
     reply: 'Tại cậu rất tuyệt vời và gấc đáng iu :3',
-    mess: 'Tớ biết mà (～￣▽￣)～. Iu cậu nhìu lémm ó hehe o((>ω< ))o',
+    mess: 'Tớ biết mà～. Iu cậu nhìu lémm ó hehe o((>ω< ))o',
     messDesc: 'Chúc mừng 19/11 nha các chàng trai ><',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://forms.gle/hC2HTGdVP9nmWGuD8' //link mess của các bạn. VD: https://m.me/nam.nodemy
